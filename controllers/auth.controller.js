8b545410fba4dde34de98fdf38a56073383cc7e4
TRUNG LÊ VĂN <18120622@student.hcmus.edu.vn>
2020-12-22 10:17:45 +0700

    fix navbar homepage

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,117 +2,118 @@ const mongoose = require('mongoose');
 const studentModel = require('../models/student.model');
 const bcrypt = require('bcryptjs');
 
-
 module.exports = {
-  login: function(req, res) {
-    if(req.headers.referer && req.headers.referer !== "http://localhost:3000/account/login") {
-      req.session.retURL = req.headers.referer;
-    }
-    console.log(req.session.retURL);
-    const status = req.query.status;
-    if(req.session.isAuth === true) {
-      let url = req.session.retURL || '/';
-      res.redirect(url);
-      return;
-    }
-    res.render('auth/login', {
-      layout: false,
-      status: status,
-    });
-  },
+    login: function (req, res) {
+        if (
+            req.headers.referer &&
+            req.headers.referer !== 'http://localhost:3000/account/login'
+        ) {
+            req.session.retURL = req.headers.referer;
+        }
+        console.log(req.session.retURL);
+        const status = req.query.status;
+        if (req.session.isAuth === true) {
+            let url = req.session.retURL || '/';
+            res.redirect(url);
+            return;
+        }
+        res.render('auth/login', {
+            // layout: true,
+            status: status,
+        });
+    },
 
-  postLogin: async function(req, res) { 
-    const username = req.body.namelogin;
-    const password = req.body.password;
-    const user = await studentModel.findByNameLogin(username);
-    if(!user) {
-      res.render('auth/login', {
-        layout: false,
-        msg: "User does not exist!",
-      });
-      return;
-    }
-    if(bcrypt.hashSync(password, user.password)) {
-      req.session.isAuth = true;
-      req.session.authUser = user;
+    postLogin: async function (req, res) {
+        const username = req.body.namelogin;
+        const password = req.body.password;
+        const user = await studentModel.findByNameLogin(username);
+        if (!user) {
+            res.render('auth/login', {
+                // layout: true,
+                msg: 'User does not exist!',
+            });
+            return;
+        }
+        if (bcrypt.hashSync(password, user.password)) {
+            req.session.isAuth = true;
+            req.session.authUser = user;
 
-      console.log(req.session.isAuth);
-      
-      let url = req.session.retURL || '/';
+            console.log(req.session.isAuth);
 
-      res.redirect(url);
-      return;
-    } else {
-      res.render('auth/login', {
-        layout: false,
-        msg: "Wrong password !",
-      });
-      return;
-    }
-  },
+            let url = req.session.retURL || '/';
 
-  postSignUp: async function(req, res) {
-    const newStudent = {
-      _id: mongoose.Types.ObjectId(),
-      namelogin: req.body.namelogin,
-      fullname: req.body.fullname,
-      password: bcrypt.hashSync(req.body.password, 10),
-      email: req.body.email,
-      date_of_birth: req.body.date_of_birth,
-      avatar: '/public/static/images/unnamed.png',
-      wishlist: [], 
-    }
-   
-    await studentModel.insertOne(newStudent);
-    var msg = encodeURIComponent('success');
-    res.redirect('/account/login/?status=' + msg);
-  },
+            res.redirect(url);
+            return;
+        } else {
+            res.render('auth/login', {
+                // layout: true,
+                msg: 'Wrong password !',
+            });
+            return;
+        }
+    },
 
-  isAvailable: async function(req, res) {
-    const username = req.query.user;
-    const user = await studentModel.findByNameLogin(username);
-    if(user === null) {
-      res.json(true);
-    } else {
-      res.json(false);
-    }
-  },
+    postSignUp: async function (req, res) {
+        const newStudent = {
+            _id: mongoose.Types.ObjectId(),
+            namelogin: req.body.namelogin,
+            fullname: req.body.fullname,
+            password: bcrypt.hashSync(req.body.password, 10),
+            email: req.body.email,
+            date_of_birth: req.body.date_of_birth,
+            avatar: '/public/static/images/unnamed.png',
+            wishlist: [],
+        };
 
-  postLogout: function(req, res) {
-    req.session.isAuth = false;
-    req.session.user = null;
-    let url = req.headers.referer;
-    res.redirect(url);
-  },
+        await studentModel.insertOne(newStudent);
+        var msg = encodeURIComponent('success');
+        res.redirect('/account/login/?status=' + msg);
+    },
 
-  profile: function(req, res) {
-    var status = req.query.status;
-    res.render('user/profile', {
-      status: status,
-    });
-  },
+    isAvailable: async function (req, res) {
+        const username = req.query.user;
+        const user = await studentModel.findByNameLogin(username);
+        if (user === null) {
+            res.json(true);
+        } else {
+            res.json(false);
+        }
+    },
 
-  editProfile: function(req, res) {
-    res.render('user/editProfile');
-  },
+    postLogout: function (req, res) {
+        req.session.isAuth = false;
+        req.session.user = null;
+        let url = req.headers.referer;
+        res.redirect(url);
+    },
 
-  postEditProfile: async function(req, res) {
-    console.log(req.body);
-    console.log(req.session.authUser.namelogin);
-    const filter = { namelogin: req.session.authUser.namelogin };
-    const update = {
-      fullname: req.body.fullname,
-      email: req.body.email,
-      date_of_birth: req.body.date_of_birth,
-    }
-    if(req.file) {
-      update.avatar = '\\' + req.file.path;
-    }
-    console.log(update);
-    const newUser = await studentModel.findOneAndUpdate(filter, update);
-    req.session.authUser = newUser;
-    var msg = encodeURIComponent('updatesuccess');
-    res.redirect('/account/profile/?status=' + msg);
-  }
-}
+    profile: function (req, res) {
+        var status = req.query.status;
+        res.render('user/profile', {
+            status: status,
+        });
+    },
 
+    editProfile: function (req, res) {
+        res.render('user/editProfile');
+    },
+
+    postEditProfile: async function (req, res) {
+        console.log(req.body);
+        console.log(req.session.authUser.namelogin);
+        const filter = { namelogin: req.session.authUser.namelogin };
+        const update = {
+            fullname: req.body.fullname,
+            email: req.body.email,
+            date_of_birth: req.body.date_of_birth,
+        };
+        if (req.file) {
+            update.avatar = '\\' + req.file.path;
+        }
+        console.log(update);
+        const newUser = await studentModel.findOneAndUpdate(filter, update);
+        req.session.authUser = newUser;
+        var msg = encodeURIComponent('updatesuccess');
+        res.redirect('/account/profile/?status=' + msg);
+    },
+};
