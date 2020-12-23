bf579a41a57bb7440f4bb206c25b534dbfa3603e
TRUNG LÊ VĂN <18120622@student.hcmus.edu.vn>
2020-12-23 22:37:22 +0700

    merge code

--- models/category.model.js
+++ models/category.model.js
@@ -4,7 +4,6 @@ const { findById } = require('./course.model');
 
 const paginate = require('mongoose-paginate-v2');
 
-
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
@@ -19,7 +18,6 @@ schema.plugin(paginate);
 const Category = mongoose.model('Category', schema, 'Category');
 
 module.exports = {
-
     async loadAllCategories() {
         return await Category.find({});
     },
@@ -27,16 +25,15 @@ module.exports = {
     findById(categoryId) {
         return Category.findById(categoryId).lean();
     },
+    async selectFromOneId(id) {
+        return await Category.findById(id);
+    },
+    async loadAll() {
+        return await Category.find().lean().populate('sub_category');
+    },
+    async loadTopCategory() {
+        return await Category.find({ sub_category: { $gt: [] } })
+            .lean()
+            .populate('sub_category');
+    },
 };
-
-  async selectFromOneId(id) {
-    return await Category.findById(id);
-  },
-  async loadAll() {
-    return await Category.find().lean().populate('sub_category');
-  },
-  async loadTopCategory() {
-    return await Category.find({ sub_category: { $gt: [] } }).lean().populate('sub_category');
-  }
-}
-
