8daed604ad6b897b291383b2961455167f6ef867
TRUNG LÊ VĂN <18120622@student.hcmus.edu.vn>
2021-01-19 16:36:43 +0700

    add form validation in creating teacher account feature

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -49,8 +49,10 @@ module.exports = {
             if (course.discount && course.discount > 0) {
                 const discount = course.discount;
                 course.salePrice = course.price * (1 - discount / 100);
+                course.salePrice = +course.salePrice.toFixed(2);
                 course.isDiscount = true;
             }
+            console.log(course);
         }
 
         const mostViewCourses = await courseModel.loadTenViewCourses();
